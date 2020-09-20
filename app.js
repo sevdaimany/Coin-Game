b99224a5c9c24590a11c8cbd2af5f1499986a5ee
sevda <imanysevda@gmail.com>
2020-09-20 19:39:20 +0430

    add arrow keys eventListener

--- app.js
+++ app.js
@@ -9,3 +9,36 @@ function isTouching(a, b) {
 		aRect.left > bRect.left + bRect.width
 	);
 }
+
+const avatar = document.querySelector("#player");
+
+window.addEventListener("keyup", function(e){
+	if(e.key === "ArrowDown" || e.key === "Down"){
+		const currTop  = extractPos(avatar.style.top);
+		avatar.style.top =`${currTop + 50}px`;
+	}
+
+	else if(e.key === "ArrowUp" || e.key === "Up"){
+		const currTop  = extractPos(avatar.style.top);
+		avatar.style.top =`${currTop - 50}px`;
+	}
+
+	else if(e.key === "ArrowRight" || e.key === "Right"){
+		const currLeft  = extractPos(avatar.style.left);
+		avatar.style.left =`${currLeft + 50}px`;
+		avatar.style.transform = 'scale(1,1)'
+	}
+
+	else if(e.key === "ArrowLeft" || e.key === "Left"){
+		const currLeft  = extractPos(avatar.style.left);
+		avatar.style.left =`${currLeft - 50}px`;
+		avatar.style.transform = 'scale(-1,1)'
+	}
+
+
+});
+
+const extractPos = (pos) => {
+	if (!pos) return 100;
+	return parseInt(pos.slice(0,-2));
+};
