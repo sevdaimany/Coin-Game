6b006a87f1517e12549f3ad334968e905699e368
sevda <imanysevda@gmail.com>
2020-09-20 16:00:14 +0430

    start coin game project

---
+++ app.js
@@ -0,0 +1,11 @@
+function isTouching(a, b) {
+	const aRect = a.getBoundingClientRect();
+	const bRect = b.getBoundingClientRect();
+
+	return !(
+		aRect.top + aRect.height < bRect.top ||
+		aRect.top > bRect.top + bRect.height ||
+		aRect.left + aRect.width < bRect.left ||
+		aRect.left > bRect.left + bRect.width
+	);
+}
